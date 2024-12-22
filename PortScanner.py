945e2c71772a09878dc660ebae960f7858965c8a
Paweł Kurek <pawel.kurek975@gmail.com>
2024-12-22 21:18:07 +0100

    Modification of messages, added termColor to messages

--- PortScanner.py
+++ PortScanner.py
@@ -3,6 +3,7 @@ import socket
 import termcolor
 
 def scan(target, ports):
+    print(f"Starting Scan For {target}:\n")
     for port in range(1, ports):
         scan_port(target, port)
 
@@ -10,16 +11,16 @@ def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
         sock.connect((ipaddress, port))
-        print(f"[+] Port Opened {ipaddress}:{port}")
+        print(f"\t[+] Port Opened {ipaddress}:{port}")
         sock.close()
     except:
-        print(f"[-] Port closed {ipaddress}:{port}")
+        pass
 
 targets = input("[*] Enter Targets To Scan (Split them by ','): ")
 ports = int(input("[*] Enter How Many Ports Do You Want To Scan: "))
 
 if ',' in targets:
-    print(f"[*] Scanning Multiple Targets")
+    print(termcolor.colored(f"\n[*] Scanning Multiple Targets\n",'green'))
     for ip_addr in targets.split(","):
         scan(ip_addr.strip(' '), ports)
 else:
